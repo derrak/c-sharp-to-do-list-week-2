92e28f7d5f5963fb0961dd7b0d3d4e8b36e1811d
Derrak Richard <derrak@gmail.com>
2022-07-21 11:53:36 -0700

    completed up to lesson 39; objects within objects, static content, and layouts and partials
    Co-authored-by: matt wilkinson <wilkinsonmatt01@gmail.com>
    Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -7,17 +7,18 @@ namespace ToDoList.Controllers
   public class ItemsController : Controller
   {
 
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      return View();
+      Category category = Category.Find(categoryId);
+      return View(category);
     }
 
     [HttpPost("/items")]
@@ -40,5 +41,16 @@ namespace ToDoList.Controllers
       Item foundItem = Item.Find(id);
       return View(foundItem);
     }
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
+    {
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
+    }
+
   }
 }
